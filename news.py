513685f8bfdbbfe2d5359af00c49369be689ae85
feysal-Ibrahim <addictivefazman@gmail.com>
2018-08-30 16:45:45 +0300

    create static,template folders and __init__.py,views.py,config.py files

--- news.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "<h1>NEWS IS COMING,STAY TUNED</h1>"
-    
-if __name__ =="__main__":
-    app.run(debug=True) 
