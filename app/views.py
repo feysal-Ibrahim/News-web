b46b00db9fb18abaa94d47b6dedf483b79cf22f0
feysal-Ibrahim <addictivefazman@gmail.com>
2018-08-30 18:00:21 +0300

    add a new route that have a news view function.

--- app/views.py
+++ app/views.py
@@ -3,4 +3,8 @@ from  app import app
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/news/<int:id>')
+def news(id):
+  return render_template('news.html',id = id) 
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from app import app
 
-
-if __name__ =='_main_':
+if __name__ =='__main__':
     app.run(debug = True)
