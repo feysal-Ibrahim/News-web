7d804f43a9930e486ce9cc6282e03652b130f57f
feysal-Ibrahim <addictivefazman@gmail.com>
2018-09-01 23:49:26 +0300

    add Debug and set it to true in your app config file

--- app/config.py
+++ app/config.py
@@ -1,15 +1,17 @@
 class Config:
-
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?{}&apiKey={}'
     ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+
+
 '''
 store the news base url inside app.config file.
 NB/we use {} to rep sections in the url that will be replaced with actual values.
 '''
 
 
-class ProdConfig (Config):
+class ProdConfig( Config ):
     pass
 
-class DevConfig (Config):
+
+class DevConfig( Config ):
     Debug = True
