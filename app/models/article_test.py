33ead6d14b77ecb6977f3a9347ab65db85dcda1a
feysal-Ibrahim <addictivefazman@gmail.com>
2018-09-03 16:23:42 +0300

    add two attribute to the article class

--- app/models/article_test.py
+++ app/models/article_test.py
@@ -11,7 +11,7 @@ class NewsTest(unittest.TestCase):
     def setUp(self): #setup method that will run before every test
 
 
-        self.new_article = Article('12','github','is good','http://abcnews.go.com','url','date')
+        self.new_article = Article('12','github','is good','http://abcnews.go.com','people','www.image.com', 'date','feysal')
 
 
     def test_instance(self):
